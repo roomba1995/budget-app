2fd37ddc3fcf39c943510fda4058f279c6bac828
roomba1995 <disgaea.keron@gmail.com>
2026-04-02 12:46:29 +0900

    feat: hide breakdown panels in budget mode SummarySection

--- app/page.tsx
+++ app/page.tsx
@@ -359,7 +359,7 @@ function AppPage({ mode }: { mode: Mode }) {
         {activeTab === "hotels" && (
           <>
             {/* Summary */}
-            <SummarySection hotels={hotels} roomChargeDb={roomChargeDb} meetingRoomDb={meetingRoomDb} overallBudgetTotal={28_929_665_120} />
+            <SummarySection hotels={hotels} roomChargeDb={roomChargeDb} meetingRoomDb={meetingRoomDb} overallBudgetTotal={28_929_665_120} hideBreakdown={mode === "budget"} />
 
             {/* Group Filter */}
             <div className="flex flex-wrap items-center gap-2">
